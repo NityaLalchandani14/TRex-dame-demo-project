01395fa069b600ae90324cd6e49043121a6f30fe
NityaLalchandani14 <86710408+NityaLalchandani14@users.noreply.github.com>
2021-07-05 12:54:32 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var sum = 0
+for (i=0; i<=9; i=i+1){
+  sum= sum + weight [i] 
+}
+console.log (sum)
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
